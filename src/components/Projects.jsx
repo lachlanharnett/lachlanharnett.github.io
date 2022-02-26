c39af6441879f0518cddb019340176e523ce8d3b
Lachlan Harnett <lachlanjharnett@gmail.com>
2022-02-26 14:24:58 +1000

    Changed amount of text display on smaller devices.

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -35,7 +35,9 @@ function Projects() {
                             Busting is a tool that allows you to find the nearest toilet when in need. It was created by
                             my team and I during the 2020 University of Queensland Computing Society Hackathon, where it
                             won the most philanthropic prize.
-
+                        </p>
+                        <p className="project-text-short">
+                            Busting is a tool that allows you to find the nearest toilet when in need.
                         </p>
                     </div>
                 </div>
@@ -50,22 +52,24 @@ function Projects() {
                     <div className="col-5">
                         <div className="row align-items-center">
                             <div className="col-lg-8 col-md-12">
-                                <h2 className="font-weight-light">Busting</h2>
+                                <h2 className="font-weight-light">Project</h2>
                             </div>
                             <div className="col-lg-2 col-auto">
-                                <a href="https://github.com/lachlanharnett/UQCS-Hackathon-2020" title="View repository">
+                                <a href="https://github.com/lachlanharnett" title="View repository">
                                     <i className="fab fa-github busting-github fa-3x project-link"></i>
                                 </a>
                             </div>
                             <div className="col-lg-2 col-auto">
-                                <a href="https://lachlanharnett.github.io/UQCS-Hackathon-2020/" title="View web app">
+                                <a href="https://github.com/lachlanharnett" title="View web app">
                                     <i className="fas fa-external-link-alt fa-3x project-link"></i>
                                 </a>
                             </div>
                         </div>
                         <p className="project-text">
-                            Lorem Ipsum is simply dummy text of the printing and
-
+                            Check back later to see my next project.
+                        </p>
+                        <p className="project-text-short">
+                            Check back later to see my next project.
                         </p>
                     </div>
                 </div>
@@ -96,6 +100,9 @@ function Projects() {
                         <p className="project-text">
                             A portfolio site to showcase my projects built using React and BootstrapJS.
 
+                        </p>
+                        <p className="project-text-short">
+                            A portfolio site to showcase my projects built using React and BootstrapJS.
                         </p>
                     </div>
                 </div>
