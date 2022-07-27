effb9e02e539007ed083fe7854edb0cfaaf24555
lachlanharnett <lachlanjharnett@gmail.com>
2022-07-27 21:14:52 +1000

    Aligned brackets better in About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,15 +5,15 @@ function About() {
         <div className="about">
             <div className="container central d-flex align-items-center justify-content-center full-background">
                 <div className ="row justify-content-center align-items-center">
-                    <div className="col-2 justify-content-center">
+                    <div className="col-2 justify-content-center align-items-center">
                         <h1 className="bracket text-center">&#123;</h1>
                     </div>
-                    <div className="col-8 justify-content-center">
+                    <div className="col-8 justify-content-center align-items-center">
                         <p className="about-text">
                             I'm a Computer Science student at The University of Queensland, graduating 2023. I'm majoring in Cybersecurity and have a keen interest in all things Computer Science and Software Engineering.
                         </p>
                     </div>
-                    <div className="col-2 justify-content-center">
+                    <div className="col-2 justify-content-center align-items-center">
                         <h1 className="bracket text-center">&#125;</h1>
                     </div>
                 </div>
