7ec65ff55473defd1c7e1985a9c7f0f304cee6b8
lachlanharnett <lachlanjharnett@gmail.com>
2022-07-27 20:22:39 +1000

    Changed tab title.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,9 +9,8 @@ function About() {
                         <h1 className="bracket text-center">&#123;</h1>
                     </div>
                     <div className="col-8">
-                        <h1 className="font-weight-light">Hi, I'm Lachlan</h1>
                         <p className="about-text">
-                            I'm a Computer Science student at the University of Queensland, graduating 2023. I'm majoring in Cybersecurity
+                            I'm a Computer Science student at the University of Queensland, graduating 2023. I'm majoring in Cybersecurity and have a keen interest in all things Software Engineering.
                         </p>
                     </div>
                     <div className="col-2">
